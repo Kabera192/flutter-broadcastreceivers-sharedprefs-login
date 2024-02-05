5ed994b3a2b188de1e03275a587ac9a385703c9d
KaberaClapton <clapton.kabera@amalitech.org>
2024-02-05 20:04:28 +0200

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:calculator_app/calculator_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Calc',
+      theme: ThemeData(
+        primaryColor: Colors.purple[900],
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
