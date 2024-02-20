2c7e6e90428fc995263b6e33add3d708b9d6bc30
KaberaClapton <clapton.kabera@amalitech.org>
2024-02-20 12:15:25 +0200

    Added broadcast receivers, shared prefs and login

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:calculator_app/calculator_view.dart';
+// ignore_for_file: prefer_const_constructors
 
-void main() {
-  runApp(const MyApp());
+import 'package:calculator_app/about.dart';
+import 'package:calculator_app/calc_theme.dart';
+import 'package:calculator_app/dependency_injection.dart';
+import 'package:calculator_app/home_page.dart';
+import 'package:calculator_app/login_page.dart';
+import 'package:calculator_app/navigation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'calculator_view.dart';
+//import 'package:calculator_app/calculator_view.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(ChangeNotifierProvider(
+      child: MyApp(),
+      create: (BuildContext context) => ThemeProvider(
+            isDarkMode: prefs.getBool("isDarkTheme") ?? false,
+          )));
+  DependencyInjection.init();
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +30,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Calc',
-      theme: ThemeData(
-        primaryColor: Colors.purple[900],
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const CalculatorView(),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return GetMaterialApp(
+          title: 'Flutter Calc',
+          theme: themeProvider.getTheme,
+          debugShowCheckedModeBanner: false,
+          home: Navigation(),
+          routes: {
+            '/homePage': (context) => HomePage(),
+            '/calcPage': (context) => CalculatorView(),
+            '/aboutPage': (context) => About(),
+            '/loginPage': (context) => LoginPage(),
+          },
+        );
+      },
     );
   }
 }
